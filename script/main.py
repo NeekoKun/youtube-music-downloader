61402a69ef9ac855352348a3cfd541bf1ba3a828
NeekoKun <leonardo.riccim@gmail.com>
2024-02-14 00:03:08 +0100

    Extended search queries

--- script/main.py
+++ script/main.py
@@ -1,18 +1,16 @@
-import yt_dlp
+import os
 from youtubesearchpython import VideosSearch
 
 def download_audio(link):
-  with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': '%(title)s.mp3'}) as video:
-    info_dict = video.extract_info(link, download = True)
-    video_title = info_dict['title']
-    print(video_title)
-    video.download(link)    
-    print("Successfully Downloaded - see local folder on Google Colab")
+    os.system("yt-dlp --extract-audio " + link)
 
 def title_to_url(title):
-    result = VideosSearch(title, limit=1)
+    results = VideosSearch(title, limit=10).result()
 
-    return result.result()['result'][0]['link']
+    for i, result in enumerate(results['result']):
+        print(str(i+1)+". "+result['title'])
+        
+    return results['result'][int(input("Input song index:\n"))-1]['link']
 
 try:
     download_audio(title_to_url(input("Input the song title:\n")))
